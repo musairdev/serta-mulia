77defa06818358370bb71185ec9845a4e3e2e242
Muhammad Saifullah Irfanuddin <musairdev@gmail.com>
2024-05-06 22:00:42 +0700

    Update storeData.js

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -3,7 +3,7 @@ const { Firestore } = require('@google-cloud/firestore');
 async function storeData(id, data) {
   const db = new Firestore();
  
-  const predictCollection = db.collection('prediction');
+  const predictCollection = db.collection('predictions');
   return predictCollection.doc(id).set(data);
 }
  
